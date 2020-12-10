f7722d330c05b5d1728404e77c0835b83b9fa7ec
tiger440 <noreddine.bek@gmail.com>
2020-12-10 10:26:29 +0100

    planning/dayschedule working + team modal into components

--- src/components/DaySchedule.vue
+++ src/components/DaySchedule.vue
@@ -1,38 +1,41 @@
 <template>
   <div>
     <div v-for="user in users" :key="user.id">
-    {{ eventSettings }} <ejs-schedule class="schedule" :selectedDate='selectedDate' :eventSettings='eventSettings' :showHeaderBar='showHeaderBar'></ejs-schedule>
+      {{ eventSettings }}
+      <ejs-schedule
+        class="schedule"
+        :selectedDate="selectedDate"
+        :eventSettings="eventSettings"
+        :showHeaderBar="showHeaderBar"
+      ></ejs-schedule>
     </div>
   </div>
-  
 </template>
 
 <script>
-import { Day, TimelineViews, DragAndDrop } from '@syncfusion/ej2-vue-schedule'
+import { Day, TimelineViews } from "@syncfusion/ej2-vue-schedule";
 /* import { scheduleData } from './datasource.js' */
 
-let scheduleData = []
+let scheduleData = [];
 
 export default {
   name: "dayschedule",
-  props: [
-    "users",
-  ],
-  data (){
+  props: ["users"],
+  data() {
     return {
-      showHeaderBar: true,
+      showHeaderBar: false,
       eventSettings: {
-          dataSource: scheduleData
+        dataSource: scheduleData,
       },
       selectedDate: new Date(2020, 10, 27),
-    }
+    };
   },
   created() {
     this.axios
-      .get('http://localhost:3000/task/findUserTasks/' + this.$route.params.id )
+      .get("http://localhost:3000/task/findUserTasks/" + this.$route.params.id)
       .then((res) => {
-        for(let i = 0; i <= res.data.length; ++i) {
-          scheduleData.push(res.data[i])
+        for (let i = 0; i < res.data.length; ++i) {
+          scheduleData.push(res.data[i]);
         }
       })
       .catch((err) => {
@@ -40,20 +43,18 @@ export default {
       });
   },
   provide: {
-    schedule: [Day, TimelineViews, DragAndDrop]
-  }
-}
+    schedule: [Day, TimelineViews],
+  },
+};
 </script>
 
 <style scoped>
-
-@import '../../node_modules/@syncfusion/ej2-base/styles/material.css';
-@import '../../node_modules/@syncfusion/ej2-buttons/styles/material.css';
-@import '../../node_modules/@syncfusion/ej2-calendars/styles/material.css';
-@import '../../node_modules/@syncfusion/ej2-dropdowns/styles/material.css';
-@import '../../node_modules/@syncfusion/ej2-inputs/styles/material.css';
-@import '../../node_modules/@syncfusion/ej2-navigations/styles/material.css';
-@import '../../node_modules/@syncfusion/ej2-popups/styles/material.css';
-@import '../../node_modules/@syncfusion/ej2-vue-schedule/styles/material.css';
-
+@import "../../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-calendars/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-popups/styles/material.css";
+@import "../../node_modules/@syncfusion/ej2-vue-schedule/styles/material.css";
 </style>
--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -1,35 +1,33 @@
 <template>
   <div>
-      <day-schedule :scheduleData="scheduleData" :users="users" class="1schedule"/>
+    <day-schedule :scheduleData="scheduleData" :users="users" class="1schedule" />
   </div>
 </template>
 
 <script>
-import DaySchedule from '../components/DaySchedule'
+import DaySchedule from "../components/DaySchedule";
 export default {
-    name: 'planning',
-    data () {
-        return {
-            users: {},
-        }
-    },
-    components: {
-        DaySchedule,
-    },
-    created() {
-      this.axios
-        .get('http://localhost:3000/team/displayTeam/' + this.$route.params.id )
-        .then((res) => {
-          this.users = res
-        })
-        .catch((err) => {
-          alert(err);
-        });
-    }
-}
+  name: "planning",
+  data() {
+    return {
+      users: {},
+    };
+  },
+  components: {
+    DaySchedule,
+  },
+  created() {
+    this.axios
+      .get("http://localhost:3000/team/displayTeam/" + this.$route.params.id)
+      .then((res) => {
+        console.log(res);
+        this.users = res;
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  },
+};
 </script>
 
-
-<style>
-
-</style>
+<style></style>
